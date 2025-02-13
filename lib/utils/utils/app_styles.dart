6f30a4cd6ea032fdc7a19594f2feb5d9411e065c
Alaanaser714 <alaanaser577@gmail.com>
2025-02-13 23:59:49 +0200

    add app styles

--- lib/utils/utils/app_styles.dart
+++ lib/utils/utils/app_styles.dart
@@ -2,10 +2,59 @@ import 'package:dashboard/utils/app_funcation/app_function.dart';
 import 'package:flutter/material.dart';
 
 abstract class AppStyles {
+  static TextStyle f16w400(context) {
+    return TextStyle(
+      fontSize: AppFunction.getResponsiveFontSize(context, fontSize: 16),
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle f18w600(context) {
+    return TextStyle(
+      fontSize: AppFunction.getResponsiveFontSize(context, fontSize: 18),
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle f16w500(context) {
+    return TextStyle(
+      fontSize: AppFunction.getResponsiveFontSize(context, fontSize: 16),
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle f12w400(context) {
+    return TextStyle(
+      fontSize: AppFunction.getResponsiveFontSize(context, fontSize: 12),
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle f20w600(context) {
+    return TextStyle(
+      fontSize: AppFunction.getResponsiveFontSize(context, fontSize: 20),
+      fontWeight: FontWeight.w600,
+    );
+  }
+
   static TextStyle f24w600(context) {
     return TextStyle(
       fontSize: AppFunction.getResponsiveFontSize(context, fontSize: 24),
       fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle f14w400(context) {
+    return TextStyle(
+      fontSize: AppFunction.getResponsiveFontSize(context, fontSize: 14),
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle f20w500(context) {
+    return TextStyle(
+      fontSize: AppFunction.getResponsiveFontSize(context, fontSize: 20),
+      fontWeight: FontWeight.w500,
     );
   }
 }
