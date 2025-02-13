6bb3b0ee1731079c9be7a889c99f47561e2e37d4
Alaanaser714 <alaanaser577@gmail.com>
2025-02-14 00:22:51 +0200

    add app images

---
+++ lib/utils/app_assets.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  static const String cardReceive = "assets/images/card-receive.svg";
+
+  static const String cardSend = "assets/images/card-send.svg";
+
+  static const String category2 = "assets/images/category-2.svg";
+
+  static const String chart2 = "assets/images/chart-2.svg";
+
+  static const String convertCard = "assets/images/convert-card.svg";
+
+  static const String gallery = "assets/images/gallery.svg";
+
+  static const String logout = "assets/images/logout.svg";
+
+  static const String moneys = "assets/images/moneys.svg";
+
+  static const String setting2 = "assets/images/setting-2.svg";
+
+  static const String wallet2 = "assets/images/wallet-2.svg";
+}
